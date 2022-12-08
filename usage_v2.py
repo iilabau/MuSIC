ffed51d742866afe632328ff5126e5872de6ffca
tauseefkhan786 <38031801+tauseefkhan786@users.noreply.github.com>
2022-12-08 18:12:15 +0530

    Update usage_v2.py

--- usage_v2.py
+++ usage_v2.py
@@ -1,7 +1,12 @@
 from iilab import musicv2
 
+# dimension of scales
 dims = [(45,90), (50,100)]
+
 mdl = musicv2(scales=dims, epoch=5, channel=1, verbose=1)
+
+# path for training data
 mdl.fit(path='F:/ICDAR2019_WORDLEVEL/training')
-       
+
+# path for test data       
 y_pred, y_true = mdl.predict(path='F:/ICDAR2019_WORDLEVEL/validation_set')  
